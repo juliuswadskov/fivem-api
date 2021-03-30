0f84d65c35d500b9647e834a98d3e8a2e2c734f2
Raag2005 <oskar@raagart.dk>
2021-03-30 22:15:15 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * @maker {Raag2005}
+ * @license {GNU}
+ * @dependencies {request}
+ * Man skal have ES6 for at bruge denne her fil!
+ */
+
+// Eksemple
+// FiveM({port: 30120, host: 'dedicated.raag2005.dk'}, (info, players) => {
+//     console.log(info);
+//     console.log(players);
+// })
+
+const request = require('request');
+
+export const FiveM = (json, cb) => {
+    if (json.host == undefined) {console.log(err); return}
+    let port = json.port || 30120
+
+    request(`http://${json.host}:${port}/info.json`, (err, res, body) => {
+        if (err) {return};
+        let info = JSON.parse(body);
+        request(`http://${json.host}:${port}/players.json`, (err, res, body) => {
+            if (err) {return};
+            let players = JSON.parse(body);
+            cb(info, players)
+        })
+    })
+}
